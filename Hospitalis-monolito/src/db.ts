ef387be2e50eb09a269aa679bc47bb24ebf65ff0
ekkogoat <lucas.henao11@gmail.com>
2026-04-27 11:18:46 -0500

    Actualiza proyecto

--- Hospitalis-monolito/src/db.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectDB(): Promise<void> {
-  await mongoose.connect(process.env.MONGODB_URI as string);
-  console.log('MongoDB conectado');
-}
